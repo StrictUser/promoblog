63a5e90d5f7a3ea51945e45d13a34c399fb7cdf7
StrictUser <ermolenko198528@gmail.com>
2016-02-04 23:22:47 +0200

    First commit: This blog uses MVC-pattern. Was created a base structure of files for blog. In this project I want to use next trchnologies: PHP 5.6.12 + MySQL 5.6.26 + Apache 2.4.16 + CSS3 + HTML5 + JS (ECMAScript 5) + jQuery 1.12.0 + Ajax and some other resources.

---
+++ app/core/view.php
@@ -0,0 +1,11 @@
+<?php
+	class View
+	{
+		public function generate($content_view, $template_view, $data = null){
+			if(is_array($data)){
+				extract($data);
+			}
+
+			include 'app/view/' . $template_view;
+		}
+	}
---
+++ app/view/template_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ua">
+	<head>
+		<meta charset="utf-8">
+		<title>Промоблог - главная страница</title>
+
+		<link rel="stylesheet" type="text/css" href="/style/main.css">
+	</head>
+	<body>
+		<?php include '/app/view/' . $content_view; ?>
+
+		<script src="/js/jquery-1.12.0.js"></script>
+	</body>
+</html>
